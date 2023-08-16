96b57cd0ccead3f7d828a6a54e2cbd09ac7c357e
Manuel Morillo <manuel.morillo@tempus.com>
2023-08-16 15:44:12 +0200

    feat: changes to show captured entities (#1)
    * feat: changes to show captured entities WIP
    
    * feat: WIP to add the entities captured
    
    * chore: merge main changes
    
    * chore: remove intercept service worker
    
    * feat: reuse colors for same parameters
    
    * chore: remove unused code

--- js/show-alternative-matches.js
+++ js/show-alternative-matches.js
@@ -1,3 +1,13 @@
+const ENTITY_COLORS_PALETTE = [
+    '#9fecfe',
+    '#afffc8',
+    '#d2c3ea',
+    '#ffd1af',
+    '#ffcdf6',
+    '#dadfe3',
+    '#d7ccc8',
+    '#fffda6',
+];
 
 function showAlternativeMatches (targetElem) {
     if (targetElem.className === 'CodeMirror-code') {
@@ -11,7 +21,7 @@ function showAlternativeMatches (targetElem) {
         const alternativeMatches = originalResponse.diagnosticInfo['Alternative Matched Intents'];
 
         // create html element
-        const alternativeMatchesHtml = createAlternativeMatchesHtml(alternativeMatches);
+        const alternativeMatchesHtml = createAlternativeMatchesHtml(alternativeMatches, originalResponse.text);
         const editorElem = document.getElementsByTagName('df-json-editor')[0];
         editorElem.parentElement.insertBefore(alternativeMatchesHtml, editorElem);
     }
@@ -37,12 +47,14 @@ function getTextFromElement (element) {
 }
 
 
-function createAlternativeMatchesHtml(matches) {
+function createAlternativeMatchesHtml(matches, input) {
     const container = document.createElement('div');
     container.id = 'sfw-alt-matches-container-id';
     container.className = 'sfw-alt-matches-container';
-    const matchesHtml = []
-    matches.forEach(({ DisplayName, Score }, idx) => {
+
+    const parameterColors = assignColorsToParameters(matches);
+    const matchesHtml = [];
+    matches.forEach(({ DisplayName, Score, Parameters }, idx) => {
         let color = 'high';
         if (Score < 0.5) {
             color = 'low';
@@ -53,8 +65,9 @@ function createAlternativeMatchesHtml(matches) {
         matchesHtml.push(`<div class="sfw-alt-match">
             <span class="sfw-alt-match-name">${DisplayName}</span>
             <span class="sfw-alt-match-score ${color}">${Score.toFixed(4)}${differenceWithPrevious}</span>
-        </div>`)
-    })
+            ${createParametersHtml(Parameters, input, parameterColors)}
+        </div>`);
+    });
 
     container.innerHTML = `
       <h3 class="gmat-subhead-2">Alternative matches</h3>
@@ -68,4 +81,40 @@ function scoreDifferenceHtml(previousScore, currentScore) {
     const diff = currentScore-previousScore
     const diffColor = Math.abs(diff) < 0.1 ? 'low' : Math.abs(diff) < 0.2 ? 'medium' : 'high'
     return `<span class="sfw-alt-match-score ${diffColor}"> (${(diff).toFixed(4)})</span>`
+}
+
+function createParametersHtml(parameters, input, parameterColors) {
+    if (!parameters) {
+        return `<div class="sfw-intent-utterance">${input}</div>`;
+    }
+
+    let utteranceWithSpans = input;
+    Object.entries(parameters).forEach(([type, { original }], i) => {
+        const color = parameterColors[type];
+        utteranceWithSpans = utteranceWithSpans.replace(original,
+             `<span class="sfw-alt-match-param" style="background-color: ${color}" title="@${type}">${original}</span>`
+        );
+    });
+
+    return `<div class="sfw-intent-utterance">${utteranceWithSpans}</div>`;
+}
+
+function assignColorsToParameters(matches) {
+    const parameterColors = {};
+    matches.forEach(({ Parameters }) => {
+        if (!Parameters) return;
+
+        Object.keys(Parameters).forEach((type, i) => {
+            if (parameterColors[type]) return;
+
+            // get the first color not used by any parameter
+            let color = ENTITY_COLORS_PALETTE.find(c =>
+                Object.values(parameterColors).every(pc => pc !== c));
+            if (!color) {
+                color = ENTITY_COLORS_PALETTE[i % ENTITY_COLORS_PALETTE.length];
+            }
+            parameterColors[type] = color;
+        });
+    });
+    return parameterColors;
 }
